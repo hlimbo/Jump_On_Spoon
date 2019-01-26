3d0f4b547a46832eb02e21092e820f356c536932
Harvey Limbo <hlimbo@users.noreply.github.com>
2019-01-26 08:56:42 -0800

    WIP SensingComponent | Add some game object tags

--- Assets/Scripts/SensingComponent.cs
+++ Assets/Scripts/SensingComponent.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class SensingComponent : MonoBehaviour
 {
     private SphereCollider sensingCollider;
+    [SerializeField]
+    private List<GameObject> objectsWithinRadius = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        print(Tags.RAT);
         sensingCollider = GetComponent<SphereCollider>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        
        if(other.gameObject.tag == Tags.RAT)
-        {
+       {
+            objectsWithinRadius.Add(other.gameObject);
             print("I See Rat");
+       }
+       else if(other.gameObject.tag == Tags.CHEESE)
+       {
+            print("I See Cheese");
+            objectsWithinRadius.Add(other.gameObject);
         }
+       else if(other.gameObject.tag == Tags.PLAYER)
+       {
+            print("I See Player");
+            objectsWithinRadius.Add(other.gameObject);
+        }
+ 
+    }
+
+    public GameObject GetLastSeenObject()
+    {
+        return lastSeenObject;
     }
 }

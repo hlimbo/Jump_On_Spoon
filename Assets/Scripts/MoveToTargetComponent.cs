f3b4b551f6e722c8d343f0cf973cd9aff79f10e7
Harvey Limbo <hlimbo@users.noreply.github.com>
2019-01-25 23:46:46 -0800

    wip SensingComponent

---
+++ Assets/Scripts/MoveToTargetComponent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToTargetComponent : MonoBehaviour
+{
+    public float moveSpeed;
+    public float stoppingDistance;
+    public GameObject target;
+    private Vector3 targetDirection;
+    private Vector3 distance;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // subtract current position by target position and normalize it
+        distance = target.transform.position - transform.position;
+        if (distance.magnitude > stoppingDistance)
+        {
+            targetDirection = Vector3.Normalize(distance);
+            rb.MovePosition(transform.position + (targetDirection * moveSpeed * Time.deltaTime));
+        }
+    }
+}
---
+++ Assets/Scripts/SensingComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensingComponent : MonoBehaviour
+{
+    private SphereCollider sensingCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(Tags.RAT);
+        sensingCollider = GetComponent<SphereCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.tag == Tags.RAT)
+        {
+            print("I See Rat");
+        }
+    }
+}

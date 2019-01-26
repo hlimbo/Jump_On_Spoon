2bc0a9a87b57d5966966fce9f47a54afb4684376
Harvey Limbo <hlimbo@users.noreply.github.com>
2019-01-25 22:51:21 -0800

    add MovingComponent

--- Assets/Scripts/MovingComponent.cs
+++ Assets/Scripts/MovingComponent.cs
@@ -1,18 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed;
+    public Vector3 moveDirection;
+    [Tooltip("Time in seconds unit moves in one direction")]
+    public float paceFrequency;
+    public bool canPatrol = true;
+
+    private Rigidbody rb;
+    [SerializeField]
+    private Vector3 currentMoveDirection;
+    private bool hasCoroutineStarted = false;
+
     void Start()
     {
-        
+        currentMoveDirection = new Vector3(moveDirection.x, moveDirection.y, moveDirection.z);
+        rb = GetComponent<Rigidbody>();
+        hasCoroutineStarted = true;
+        StartCoroutine(Patrol());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Patrol()
     {
-        
+        while (canPatrol)
+        {
+            float startTime = Time.time;
+            float deltaTime = Time.time - startTime;
+            while (deltaTime < paceFrequency)
+            {
+                rb.MovePosition(transform.position + (currentMoveDirection * moveSpeed * Time.deltaTime));
+                yield return null;
+                deltaTime = Time.time - startTime;
+            }
+
+            currentMoveDirection = currentMoveDirection * -1.0f;
+        }
+    }
+
+    private void Update()
+    {
+        if(!canPatrol)
+        {
+            StopCoroutine(Patrol());
+            hasCoroutineStarted = false;
+        }
+        else if(!hasCoroutineStarted && canPatrol) 
+        {
+            hasCoroutineStarted = true;
+            StartCoroutine(Patrol());
+        }
     }
 }

81b31b985123e101f79e9753ec72491d6391eabb
Josh Coyco <jac6308@gmail.com>
2019-01-27 01:05:59 -0800

    Die Script. Better Bedroom

---
+++ Assets/Die.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Die : MonoBehaviour
+{
+    public float dieVelocity = 35f;
+    private float v;
+    private RigidbodyFirstPersonController rfpc;
+    private Rigidbody rb;
+    private void Start()
+    {
+        rfpc = GetComponent<RigidbodyFirstPersonController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(rfpc.Grounded);
+        Debug.Log(v);
+        if (rfpc.Grounded && v < -dieVelocity)
+        {
+            SpawnPoint.Respawn(transform);
+        }
+        v = rb.velocity.y;
+    }
+}

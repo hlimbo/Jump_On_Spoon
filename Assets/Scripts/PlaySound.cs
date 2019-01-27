5c3621d3e6bea1d7b9019994be916dacd5944aa4
Kristi Chang <kristi.chang@csu.fullerton.edu>
2019-01-27 15:28:28 -0800

    play sound script

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+
+	public AudioClip[] sound;
+	AudioSource audio;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        audio.clip = sound[0];
+        audio.Play();
+    }
+
+    // void OnCollisionStay(Collision collision)
+    // {
+    // 	audio.clip = sound[1];
+    //     audio.Play();
+    // }
+
+}

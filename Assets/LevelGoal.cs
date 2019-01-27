e4382902584fbb084080913815579d2904987e9c
hlimbo <hlimbo@users.noreply.github.com>
2019-01-27 15:18:41 -0800

    pls add them

---
+++ Assets/LevelGoal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class LevelGoal : MonoBehaviour
+{
+    GameManager gm;
+    private void Awake()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.GetComponent<RigidbodyFirstPersonController>())
+            gm.LoadNextScene();
+        Debug.Log("Triggered");
+    }
+}
---
+++ Assets/ToiletBowlFlush.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletBowlFlush : MonoBehaviour
+{
+    public AudioClip flushSound;
+    public GameObject blackCurtainPrefab;
+
+    private AudioSource aSource;
+    private bool flushed = false;
+
+    private void Awake ()
+    {
+        aSource = GetComponent<AudioSource> ();
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!flushed)
+            {
+                StartCoroutine (Flush (other.transform));
+                flushed = true;
+            }
+        }
+    }
+
+    private IEnumerator Flush (Transform t)
+    {
+        aSource.PlayOneShot (flushSound);
+
+        Instantiate (blackCurtainPrefab, t.position + t.forward * 5f, t.rotation, t);
+        float blackTime = 2f;
+        float start = Time.time;
+
+        yield return new WaitForSeconds (blackTime);
+
+        GameManager gm = FindObjectOfType<GameManager> ();
+        if (gm)
+        {
+            gm.LoadNextScene ();
+        }
+    }
+
+}

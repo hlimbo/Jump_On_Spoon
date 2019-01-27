abe6a1b1d739cbeefebbd75211dcc5933b2ea155
Pitabreadgamedev <peterdcsulb16@gmail.com>
2019-01-27 06:23:55 -0800

    Bathroom is beatable. Needs spawnpoints and trigger exiting scene to be setup. Paper towel tower is unruly and a little tedious. If someone gets time and an UNBELIEVABLY EASY idea to implement that gets the player on top of the cabinet. More power to you. (2) Implemented "climbing mechanic" basically if you're just out of reach of jumping on top of something, you'll get a little boost over the edge to ease platforming difficulty. (3) Made max holdOffset clamp "5f" as opposed to 3 so the toilet paper didn't take up the whole screen. (4) Removed rat stuff.

--- Assets/Scripts/PickupController.cs
+++ Assets/Scripts/PickupController.cs
@@ -69,7 +69,7 @@ public class PickupController : MonoBehaviour
                 holdOffset += .25f;
             else if (Input.GetAxis("Mouse ScrollWheel") < 0)
                 holdOffset -= .25f;
-            holdOffset = Mathf.Clamp(holdOffset, 0f, 3f);
+            holdOffset = Mathf.Clamp(holdOffset, 0f, 5f);
             hold(ray.GetPoint(4f + holdOffset));
         }
     }

2675acf819eabf9208c2e7cd447720ae6166fe9e
Josh Coyco <jac6308@gmail.com>
2019-01-26 22:56:16 -0800

    no message

---
+++ Assets/Ellie.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Ellie : MonoBehaviour
+{
+    Animator anim;
+    const string RUNNING = "Running";
+    const string WALKING = "Walking";
+    private RigidbodyFirstPersonController rpfc;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rpfc = GetComponentInParent<RigidbodyFirstPersonController>();
+
+    }
+
+    void Update()
+    {
+        anim.SetBool(WALKING, (rpfc.Velocity.sqrMagnitude > 0f));
+        anim.SetBool(RUNNING, rpfc.Running);
+    }
+}

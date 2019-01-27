79bf56b619bbd21beefc90500a2c2da1ea2334da
hlimbo <hlimbo@users.noreply.github.com>
2019-01-27 08:29:29 -0800

    Add navmeshcomponents experimental

---
+++ Assets/Scripts/NavMeshMoveTo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshMoveTo : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = destination.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
